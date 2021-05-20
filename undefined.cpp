27b6406cddecc141bdeacee2002afe08671dbb25
riyai <72334955+riyai@users.noreply.github.com>
2021-05-20 20:44:07 +0530

    Update undefined.cpp
    Code to find max and min different letters in two string inputs of the same length.
    Input
    2
    a?c
    ??b
    ???a
    ???a
    Output
    1 3
    0 3
    CODECHEF CHEFSTLT

--- undefined.cpp
+++ undefined.cpp
@@ -10,19 +10,19 @@ int main() {
 	vector <string> s2;
 	string a, b;
 	cin >> t;
-	for(int i=0; i<t; i++){
+	for (int i=0; i<t; i++){
 	     cin >> a;
 	     s1.push_back(a);
 	     cin >> b;
 	     s2.push_back(b);
 	}
 	int s;
-	for(int i=0; i<t; i++){
+	for (int i=0; i<t; i++){
 	    s = s1[i].size();
-	    for(int j=0; j<s; j++){
-	        if(s1[i][j]=='?'||s2[i][j]=='?')
+	    for (int j=0; j<s; j++){
+	        if (s1[i][j]=='?'||s2[i][j]=='?')
 	            max++;
-	        else if(s1[i][j]!=s2[i][j]){
+	        else if (s1[i][j]!=s2[i][j]){
 	            min++;
 	            max++;
 	        }
